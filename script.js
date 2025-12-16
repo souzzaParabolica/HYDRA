5acb9a2be11cd0ea481f8e6af55e9289af278011
souzza <cauelima752@gmail.com>
2025-12-15 23:25:52 -0300

    colocando botão para voltar ao topo

--- script.js
+++ script.js
@@ -3,7 +3,33 @@ const lenis = new Lenis({
   autoRaf: true,
 });
 
-// Listen for the scroll event and log the event data
+const backToTopBtn = document.getElementById("back-to-top");
+
 lenis.on("scroll", (e) => {
-  console.log(e);
+  const scroll = e.scroll;
+
+  if (scroll > 300) {
+    gsap.to(backToTopBtn, {
+      opacity: 1,
+      scale: 1,
+      duration: 0.3,
+      ease: "power2.out",
+      pointerEvents: "auto",
+    });
+  } else {
+    gsap.to(backToTopBtn, {
+      opacity: 0,
+      scale: 0.9,
+      duration: 0.3,
+      ease: "power2.out",
+      pointerEvents: "none",
+    });
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  lenis.scrollTo(0, {
+    duration: 1,
+    easing: (t) => 1 - Math.pow(1 - t, 3),
+  });
 });
